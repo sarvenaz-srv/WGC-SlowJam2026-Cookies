daaf53b66b1797179e9eb6c21b9de531e687f0bd
Sarvenaz Sarvghad <61093799+sarvenaz-srv@users.noreply.github.com>
2026-08-26 15:52:41 -0500

    add bile sound effects

--- scenes/Bile.gd
+++ scenes/Bile.gd
@@ -3,10 +3,14 @@ extends Node2D
 var speed: float = 20.0
 var direction: Vector2 = Vector2.UP
 
+@onready var risingAudioPlayer = $BileRisingAudioStreamPlayer
+@onready var killPlayerAudioPlayer = $BileKilledPlayerAudioStreamPlayer
+
 var should_rise: bool = false
 
 func start_rising() -> void:
 	should_rise = true
+	risingAudioPlayer.play()
 
 func _process(delta: float) -> void:
 	if should_rise:
@@ -14,4 +18,5 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
+		killPlayerAudioPlayer.play()
 		GameOverMenu.show_game_over()
